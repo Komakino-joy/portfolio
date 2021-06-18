b6cc9c23a57977bf5739cb7294b3137f45e9f321
Bryan Alvarez <Bryan.Alvarez@genco.com>
2021-06-18 12:20:07 -0700

    reverted to precompiled version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
-
 import Homepage from './pages/homepage.component';
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Homepage />
       <Footer />
