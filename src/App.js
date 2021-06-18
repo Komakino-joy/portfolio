d4ec4c7d657b30aa790944834213b19478085db9
Bryan Alvarez <Bryan.Alvarez@genco.com>
2021-06-18 12:27:56 -0700

    Revert "reverted to precompiled version"
    This reverts commit b6cc9c23a57977bf5739cb7294b3137f45e9f321.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import './App.css';
+
 import Homepage from './pages/homepage.component';
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component'
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header />
       <Homepage />
       <Footer />
