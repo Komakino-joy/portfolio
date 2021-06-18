4efa39548d08cd61fb9d47c2b2a2ba07b9742ba4
Bryan Alvarez <Bryan.Alvarez@genco.com>
2021-06-18 12:17:51 -0700

    Revert "footer added"
    This reverts commit 021e71db439247c5571813ed4207d038df57e6e4.

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -8,7 +8,6 @@ const Footer = () => {
     return (
         <div className='footer-wrapper'>
             <img className='footer-image' src={personCoding} alt='person-coding'></img>
-            <footer>Thank you for stopping by...</footer>
         </div>
     )
 }
