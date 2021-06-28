de2922e1fbcffce0526e8d594ba1615490d6633a
Bryan Alvarez <Bryan.Alvarez@genco.com>
2021-06-28 15:56:05 -0700

    fixed typos

--- src/components/bio/bio.component.jsx
+++ src/components/bio/bio.component.jsx
@@ -16,7 +16,7 @@ const Bio = () => {
                 <div className='bio-text-container'>
                 <h3 className='bio-text-header'>About Me</h3>
                     <p className='bio-text-detail'>
-                    Hello, my name is Bryan Alvarez. I am a full stack web developer. I specialize on the React Framework, but also have experience with the Vue Framework and traditional (JS, CSS, HTML).  I attended college for computer information systems and computer science. I am continuously working to expand my expertise in the ever-changing world of web development. I also have a strong background in customer service, data analysis, and system administration. I am a self-starter who and enjoys facing challenging problems head on and the satisfaction that comes with finding a solution.
+                    Hello, my name is Bryan Alvarez. I am a full stack web developer. I specialize on the React Framework, but also have experience with the Vue Framework and traditional (JS, CSS, HTML).  I attended college for computer information systems and computer science. I am continuously working to expand my expertise in the ever-changing world of web development. I also have a strong background in customer service, data analysis, and system administration. I am a self-starter who enjoys facing challenging problems head on and the satisfaction that comes with finding a solution.
                     </p>
                 </div>
             </div>
